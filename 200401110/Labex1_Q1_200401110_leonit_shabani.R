0b54c8a7bb1db5e5d05a93b51aa2ea5ae776095d
Leonit Shabani <leonitshabani3@gmail.com>
2023-11-11 18:24:37 +0300

    1.2, testleri ile tamamlandı

--- 200401110/Labex1_Q1_200401110_leonit_shabani.R
+++ 200401110/Labex1_Q1_200401110_leonit_shabani.R
@@ -1,1 +1,5 @@
+# Q 1.1
 download.file("https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv", "MapsThatChangedOurWorld_StoryMap_Data.csv")
+
+# Q 1.2
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv", header=TRUE, sep=";")
---
+++ 200401110/Labex1_Q1_tests_200401110_leonit_shabani.R
@@ -0,0 +1,5 @@
+# Q 2-2
+rm(list = ls())
+if(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")){
+  file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+}
